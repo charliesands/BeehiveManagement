fdc14965904a9904dcb0c55e429441cb2dc480e5
charliesands <cwsands3@gmail.com>
2018-12-05 01:15:27 -0700

    setting classes up

--- BeehiveManagement/Worker.cs
+++ BeehiveManagement/Worker.cs
@@ -7,5 +7,23 @@ namespace BeehiveManagement
 {
     class Worker
     {
+        private string[] jobsICanDo;
+        public Worker(string[] jobsICanDo)
+        {
+            this.jobsICanDo = jobsICanDo;
+        }
+
+        private string currentJob = "";
+        public string CurrentJob
+        {
+            get
+            {
+                return currentJob;
+            }
+        }
+
+        private int shiftsToWork;
+        private int shiftsWorked;
+
     }
 }
